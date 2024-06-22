b621ab38822aab0cc162d5af503ee9f317fe458e
Aisha Najeeb <90481018+ayshanjb25@users.noreply.github.com>
2024-06-22 23:47:49 +0530

    Implement the complete home layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import RighSidebar from '@/components/ui/RighSidebar';
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: "Aisha",lastName: "Najeeb",email: "aisha@mail.com"};
   return (
     <div>
+      <section className='home'>
+        <div className='home-content'>
+          <header className='home-header'>
+            <HeaderBox type="greeting" title="Welcome" 
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficently."/>
+         <TotalBalanceBox accounts={[]} totalBanks={1}
+         totalCurrentBalance={1250.35}/>
+         
+         
+          </header>
+          RECENT TRANSACTIONS
+        </div>
+        <RighSidebar user={loggedIn} transactions={[]} banks={[{currentBalance:123.50},{currentBalance:500.50}]}/>
+      </section>
+      
       
     </div>
   )
